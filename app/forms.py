81e57cada6b2166c034a3cc497ec49a9cec33d56
Hari Hari <harshadvali1431.com>
2023-12-08 15:46:16 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from app.models import *
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['username','password','email']
+        widgets={'password':forms.PasswordInput}
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=['address','profile_pic']
+
+class WeatherDataForm(forms.ModelForm):
+    class Meta:
+        model=WeatherData
+        fields=['city']
